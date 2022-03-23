0009dfa66302f4d9aef7b46c69c9b5fec8fdd627
Saturn Maoudis <alliinnovations@gmail.com>
2022-03-23 19:03:46 +0000

    Stable: assume if question assume if has emoji Unstable: assume if statement Cannot: process questions process statements

--- Assets/proccessing/inputProccessing.js
+++ Assets/proccessing/inputProccessing.js
@@ -7,38 +7,25 @@ let assumeIncludesEmoji = false
 let currentCheck = 0
 let userInputLength = 0
 
-function checkQuestion() {
-    currentCheck = 0
-    while (currentCheck <= userInputLength) {          // VERY UNSTABLE
-        if (userInput[currentCheck] == "?") {
-            assumeQuestion = true
-            return assumeQuestion;
-        } else {
-
-        }
-        currentCheck = currentCheck + 1
+function determineInput() {
+    if (rawUserInput.includes('?')) {
+        assumeQuestion = true
+        assumeStatement = false
+        console.log('Debug: Determined if question' + assumeQuestion)
+    } else {
+        assumeQuestion = false
+        assumeStatement = true
+        console.log('Debug: Determined if question' + assumeQuestion)
     }
 }
 
-// function checkStatement() {
-//     currentCheck = 0
-//     while (currentCheck < userInputLength) {
-//         if (userInput[currentCheck] == "STATEMENT STUFF") {  // needs to import built in logic
-//             console.log('Debug: Assume statement')
-//         } else if (currentCheck == userInput.length()) {
-//             console.log('Debug: Do not assume statement')
-//             assumeQuestion = false
-//         } else {
-//             currentCheck = currentCheck + 1
-//         }
-//     }
-// }
-
 function checkIncludesEmoji() {
-    if (userInput == "") {
+    if (userInput.includes(':')) {
         assumeIncludesEmoji = true
+        console.log('Debug: Determined if includes emoji' + assumeIncludesEmoji)
     } else {
         assumeIncludesEmoji = false
+        console.log('Debug: Determined if includes emoji' + assumeIncludesEmoji)
     }
 }
 
@@ -55,7 +42,17 @@ function checkValidationValues() {
 lowerRawUserInput = rawUserInput.toLowerCase()
 userInput = lowerRawUserInput.split(' ')
 userInputLength = userInput.length()
-checkQuestion()
+determineInput()
 checkIncludesEmoji()
 checkValidationValues()
 
+if (assumeQuestion == true) {
+    processQuestion()
+    console.log('Debug: Started processing question')
+} else if (assumeStatement == true) {
+    console.log('Debug: Started processing statement')
+    processStatement()
+} else {
+    errorMessage = "Error: Could not process input (' + rawUserInput + ')"
+    console.log(errorMessage)
+}
