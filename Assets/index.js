c7c5adc8a15825c24af16327cf64485393fc3e85
Saturn Maoudis <alliinnovations@gmail.com>
2022-03-23 20:38:32 +0000

    linked index.js and inputProcessing.js with index.html started formatting inputProcessing by moving parts into index.js and leaving the functions.

--- Assets/index.js
+++ Assets/index.js
@@ -1,1 +1,30 @@
-import builtInLogic from '/Assets/logic/builtInLogic.json';
+// import 'Assets/processing/inputProcessing.js'
+// import builtInLogic from 'Assets/logic/builtInLogic.json';
+
+let rawUserInput = '' // The raw user input - import ?
+let lowerRawUserInput = 0
+let userInput = 0
+let assumeQuestion = false
+let assumeStatement = false
+let assumeIncludesEmoji = false
+let currentCheck = 0
+let userInputLength = 0
+
+
+// lowerRawUserInput = rawUserInput.toLowerCase()
+// userInput = lowerRawUserInput.split(' ')
+// userInputLength = userInput.length()
+// determineInput()
+// checkIncludesEmoji()
+// checkValidationValues()
+
+// if (assumeQuestion == true) {
+//     processQuestion()
+//     console.log('Debug: Started processing question')
+// } else if (assumeStatement == true) {
+//     console.log('Debug: Started processing statement')
+//     processStatement()
+// } else {
+//     errorMessage = "Error: Could not process input (' + rawUserInput + ')"
+//     console.log(errorMessage)
+// }
